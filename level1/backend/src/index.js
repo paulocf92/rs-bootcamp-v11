cd292b6f8f0174769ccb98d85f54076980a51ad9
Paulo Filho <pcf.feat@gmail.com>
2020-04-09 10:14:54 -0400

    lesson3: explain http methods and query params

--- level1/backend/src/index.js
+++ level1/backend/src/index.js
@@ -2,8 +2,55 @@ const express = require("express");
 
 const app = express();
 
+// Instruct express to understand JSON (middleware)
+app.use(express.json());
+
+/**
+ * HTTP methods
+ *
+ * GET: Retrieve data from backend
+ * POST: Create data in the backend
+ * PUT: Update data in the backend
+ * DELETE: Delete data from the backend
+ */
+
+/**
+ * Parameter types
+ *
+ * Query params: Filters and pagination
+ * Route params: Identify resources when updating/deleting a resource
+ * Request body: Content used when creating/updating a resource (JSON)
+ */
+
+// Query params
 app.get("/projects", (req, res) => {
-  return res.json({ message: "Hello World" });
+  const { title, owner } = req.query;
+
+  console.log({ title, owner });
+
+  return res.json(["Project 1", "Project 2", "Project 3"]);
+});
+
+// Body params
+app.post("/projects", (req, res) => {
+  const { title, owner } = req.body;
+
+  console.log({ title, owner });
+
+  return res.json(["Project 1", "Project 2", "Project 3"]);
+});
+
+// Route params
+app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log({ id });
+
+  return res.json(["Project 1", "Project 2", "Project 3"]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  return res.json(["Project 2", "Project 3"]);
 });
 
 app.listen(3333, () => {
