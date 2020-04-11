fc811930447b5e7c446f27d3400e09e03cf186cb
Paulo Filho <pcf.feat@gmail.com>
2020-04-11 10:05:38 -0400

    lesson2: add basic styles and understand the differences between react for web and react native

--- level1/mobile/src/index.js
+++ level1/mobile/src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7159c1",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
