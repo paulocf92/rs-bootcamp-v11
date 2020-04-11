b53869e63ec973d655c89147bf54398ee232cc59
Paulo Filho <pcf.feat@gmail.com>
2020-04-10 22:11:08 -0400

    lesson7: add cors to backend and pull data from the frontend

--- level1/frontend/src/App.js
+++ level1/frontend/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./App.css";
-
-import backgroundImage from "./assets/background.jpg";
 
 import Header from "./components/Header";
 
 // <></> denotes a fragment, it's a container but it has no visual feedback
 function App() {
-  const [projects, setProjects] = useState([
-    "App development",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `New project ${Date.now()}`]);
@@ -21,11 +23,9 @@ function App() {
     <>
       <Header title="Projects" />
 
-      <img width={300} src={backgroundImage} />
-
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
