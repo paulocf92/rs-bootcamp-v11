25e8d9a286b57851199aaadfb03abfce94b01bfd
Paulo Filho <pcf.feat@gmail.com>
2020-04-11 10:29:44 -0400

    lesson3: add projects list pulled from the server

--- level1/mobile/src/index.js
+++ level1/mobile/src/index.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+import api from "./services/api";
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("/projects").then((response) => setProjects(response.data));
+  }, []);
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -16,13 +34,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#7159c1",
   },
-  title: {
+  project: {
     color: "#fff",
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 20,
   },
 });
