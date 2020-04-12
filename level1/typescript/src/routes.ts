75eee8caac7f333e8e3ccf5a5a6128fa4405088a
Paulo Filho <pcf.feat@gmail.com>
2020-04-12 10:32:49 -0400

    lesson4: explain interfaces (groups of data), arrays and variating types per definition

--- level1/typescript/src/routes.ts
+++ level1/typescript/src/routes.ts
@@ -2,7 +2,17 @@ import { Request, Response } from "express";
 import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
-  const user = createUser("Paulo", "paulo@gmail.com", "123456");
+  const user = createUser({
+    name: "Paulo",
+    email: "paulo@gmail.com",
+    password: "123456",
+    techs: [
+      "NodeJS",
+      "ReactJS",
+      "React Native",
+      { title: "Javascript", experience: 100 },
+    ],
+  });
 
   return response.json(user);
 }
--- level1/typescript/src/services/CreateUser.ts
+++ level1/typescript/src/services/CreateUser.ts
@@ -2,11 +2,33 @@
  * To create an user: name, email, password
  */
 
-export default function createUser(name = "", email: string, password: string) {
+interface TechObject {
+  title: string;
+  experience: number;
+}
+
+interface CreateUserData {
+  name?: string;
+  email: string;
+  password: string;
+  /**
+   * Alternative
+   * techs: string[];
+   */
+  techs: Array<string | TechObject>;
+}
+
+export default function createUser({
+  name = "",
+  email,
+  password,
+  techs,
+}: CreateUserData) {
   const user = {
     name,
     email,
     password,
+    techs,
   };
 
   return user;
