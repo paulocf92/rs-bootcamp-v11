2378352fe129a5af36055756d46c5aab5f0aa517
Paulo Filho <pcf.feat@gmail.com>
2020-04-12 10:13:11 -0400

    lesson2: explain type usage and definition imports

--- level1/typescript/src/index.ts
+++ level1/typescript/src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
+import { helloWorld } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello GoStack" });
-});
+app.get("/", helloWorld);
 
 app.listen(3333);
