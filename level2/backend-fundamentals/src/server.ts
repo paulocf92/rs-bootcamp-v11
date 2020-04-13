d9022f6909be790edd49aec39ebdef7695cb2173
Paulo Filho <pcf.feat@gmail.com>
2020-04-13 14:10:03 -0400

    lesson3: add eslint to provide consistency among different code styles

--- level2/backend-fundamentals/src/server.ts
+++ level2/backend-fundamentals/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello GoStack!" });
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello GoStack!' });
 });
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333!");
+  console.log('Server started on port 3333!');
 });
