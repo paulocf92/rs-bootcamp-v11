b20bcfea2011757214e13030ae697afe7194a01b
Paulo Filho <pcf.feat@gmail.com>
2020-04-13 15:31:13 -0400

    feat: add routes structure and appointments route

--- level2/backend-fundamentals/src/routes/index.ts
+++ level2/backend-fundamentals/src/routes/index.ts
@@ -1,20 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack!' }),
-);
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
