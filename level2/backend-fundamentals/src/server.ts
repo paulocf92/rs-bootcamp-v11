5e26f4bec00c85c50c5b50c356f3df040d720dc8
Paulo Filho <pcf.feat@gmail.com>
2020-04-13 13:37:52 -0400

    lesson1: set up server with typescript and ts-server reload

---
+++ level2/backend-fundamentals/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello GoStack!" });
+});
+
+app.listen(3333, () => {
+  console.log("Server started on port 3333!");
+});
