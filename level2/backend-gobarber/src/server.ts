dfc98911e8620e977bbbfef4cb0cfdb3961655cc
Paulo Filho <pcf.feat@gmail.com>
2020-04-14 16:44:47 -0400

    feat: add typeorm with postgres setup

--- level2/backend-gobarber/src/server.ts
+++ level2/backend-gobarber/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333, () => {
