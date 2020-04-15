89f6b32fcf4c7bc526d9bf10544739a659d491bb
Paulo Filho <pcf.feat@gmail.com>
2020-04-15 13:51:55 -0400

    feat: add appointment-to-user relationship

--- level2/backend-gobarber/src/database/migrations/1586897369294-CreateAppointments.ts
+++ level2/backend-gobarber/src/database/migrations/1586897369294-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1586897369294
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- level2/backend-gobarber/src/database/migrations/1586965004985-CreateUsers.ts
+++ level2/backend-gobarber/src/database/migrations/1586965004985-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1586965004985 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
