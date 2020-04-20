10cd6618baa9581bb5f410825da23d4743983c51
Paulo Filho <pcf.feat@gmail.com>
2020-04-20 15:42:36 -0400

    feat: add navigation between dashboard and details page

--- level3/frontend-fundamentals/src/pages/Dashboard/index.tsx
+++ level3/frontend-fundamentals/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -76,7 +77,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a href="test" key={repository.full_name}>
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -87,7 +91,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
