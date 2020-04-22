56db04aa60dfbc4e30a2014eb5de4ad141852502
Paulo Filho <pcf.feat@gmail.com>
2020-04-22 12:20:06 -0400

    chore: add register page

--- level3/frontend-gobarber/src/App.tsx
+++ level3/frontend-gobarber/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
   );
---
+++ level3/frontend-gobarber/src/pages/SignUp/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="user" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="createAccount">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
