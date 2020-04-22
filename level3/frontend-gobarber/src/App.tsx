eb4f1ccae53f338c57697b2d66b57b4c45b0d1dd
Paulo Filho <pcf.feat@gmail.com>
2020-04-22 09:56:38 -0400

    chore(style): add global styles

--- level3/frontend-gobarber/src/App.tsx
+++ level3/frontend-gobarber/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
