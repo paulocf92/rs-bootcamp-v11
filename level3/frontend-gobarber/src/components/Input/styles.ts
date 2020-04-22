8316d2875ae448c37850e78c4f917ce15304fae1
Paulo Filho <pcf.feat@gmail.com>
2020-04-22 14:39:31 -0400

    chore(style): add errors tooltips

--- level3/frontend-gobarber/src/components/Input/styles.ts
+++ level3/frontend-gobarber/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,10 +25,16 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
-      border: 2px solid #ff9000;
+      border-color: #ff9000;
     `}
 
   ${props =>
@@ -33,6 +42,8 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
+
 
   input {
     flex: 1;
@@ -47,5 +58,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
