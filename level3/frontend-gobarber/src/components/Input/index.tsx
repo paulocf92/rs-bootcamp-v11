cfda0a67fcaa524d01a908e2085578c31b2f2fbe
Paulo Filho <pcf.feat@gmail.com>
2020-04-22 13:57:11 -0400

    feat: add errors display

--- level3/frontend-gobarber/src/components/Input/index.tsx
+++ level3/frontend-gobarber/src/components/Input/index.tsx
@@ -49,6 +49,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+      {error}
     </Container>
   );
 };
--- level3/frontend-gobarber/src/pages/SignUp/index.tsx
+++ level3/frontend-gobarber/src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,22 +14,27 @@ import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(
-          6,
-          'Sua senha deve conter pelo menos 6 dígitos',
-        ),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
-      await schema.validate(data);
+      await schema.validate(data, {
+        abortEarly: false,
+      });
     } catch (err) {
-      console.error(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -36,7 +44,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
