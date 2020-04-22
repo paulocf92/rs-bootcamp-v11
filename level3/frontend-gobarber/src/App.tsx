5e1af5a096a2d7db0b79ece55d64520567dd78a7
Paulo Filho <pcf.feat@gmail.com>
2020-04-22 10:39:12 -0400

    feat: add sign-in page

--- level3/frontend-gobarber/src/App.tsx
+++ level3/frontend-gobarber/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   );
