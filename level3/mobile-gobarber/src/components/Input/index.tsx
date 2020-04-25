607f88a80e8b1ee18454f85d0feaff917d3e01d8
Paulo Filho <pcf.feat@gmail.com>
2020-04-25 08:52:40 -0400

    chore(style): add focus/blur dynamic styles

--- level3/mobile-gobarber/src/components/Input/index.tsx
+++ level3/mobile-gobarber/src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import React, {
   useEffect,
+  useCallback,
   useRef,
   useImperativeHandle,
   forwardRef,
+  useState,
 } from 'react';
 import { TextInputProperties } from 'react-native';
 
@@ -32,6 +34,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   const { fieldName, defaultValue = '', registerField, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
@@ -55,13 +70,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [registerField, fieldName]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
