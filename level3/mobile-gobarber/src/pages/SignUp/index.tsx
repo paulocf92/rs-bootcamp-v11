dbfdfdee36a1a7dc069bff6059d247de7cfd7953
Paulo Filho <pcf.feat@gmail.com>
2020-04-25 09:29:54 -0400

    feat: enable sign up in the app

--- level3/mobile-gobarber/src/pages/SignUp/index.tsx
+++ level3/mobile-gobarber/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -57,9 +58,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você já pode fazer login na aplicação',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
