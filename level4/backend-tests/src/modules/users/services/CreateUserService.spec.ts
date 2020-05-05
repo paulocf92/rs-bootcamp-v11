8681eea814675c7cba6f4f5724cf011636fbd1ca
Paulo Filho <pcf.feat@gmail.com>
2020-05-05 17:24:24 -0400

    test: add authentication service tests and refactor password encryption into an interface

--- level4/backend-tests/src/modules/users/services/CreateUserService.spec.ts
+++ level4/backend-tests/src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,18 @@
 import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -18,7 +25,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with an existing email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
