2a8e7ba5abb7edbba391d0093cd5238c368436ab
Paulo Filho <pcf.feat@gmail.com>
2020-05-05 09:57:44 -0400

    refactor: further improve code by applying interfaces

--- level4/backend-tests/src/modules/users/services/UpdateUserAvatarService.ts
+++ level4/backend-tests/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,22 +1,22 @@
-import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
 
-interface Request {
+interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const user = await usersRepository.findOne(user_id);
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('Only authenticated users may change avatar!', 401);
@@ -36,7 +36,7 @@ class UpdateUserAvatarService {
     // Set new filename (create or update)
     user.avatar = avatarFilename;
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
