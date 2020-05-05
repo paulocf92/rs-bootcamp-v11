ba3b6c471c1441b9ab116e38759ae7114ee3cf7c
Paulo Filho <pcf.feat@gmail.com>
2020-05-05 15:26:46 -0400

    test: add appointment creation test

--- level4/backend-tests/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ level4/backend-tests/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,6 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
+
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
@@ -8,8 +10,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
