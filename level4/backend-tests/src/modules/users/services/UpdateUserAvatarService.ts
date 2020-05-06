c7843dead3579249f19eac5702d66fa1ff1f4666
Paulo Filho <pcf.feat@gmail.com>
2020-05-06 15:34:49 -0400

    chore: add storage provider interface and fake storage provider

--- level4/backend-tests/src/modules/users/services/UpdateUserAvatarService.ts
+++ level4/backend-tests/src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import uploadConfig from '@config/upload';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
@@ -18,6 +19,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -29,17 +33,13 @@ class UpdateUserAvatarService {
 
     // On user avatar existence
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        // Delete it
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
     // Set new filename (create or update)
-    user.avatar = avatarFilename;
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
