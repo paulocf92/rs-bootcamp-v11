b797b12b2ad2e730436cd4d035b015b38c4d929f
Paulo Filho <pcf.feat@gmail.com>
2020-05-13 10:24:24 -0400

    feat: add providers monthly availability listing

---
+++ level4/backend-gobarber-finish/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,47 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+// import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+// import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    console.log(appointments);
+
+    return [{ day: 1, available: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
