ff17f2b65f435b421c80def797cd5cb8255d8840
Paulo Filho <pcf.feat@gmail.com>
2020-05-15 15:24:58 -0400

    feat: add backend validation on routes

--- level4/backend-gobarber-finalize/src/modules/users/infra/http/routes/users.routes.ts
+++ level4/backend-gobarber-finalize/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UserAvatarController from '../controllers/UserAvatarController';
 
@@ -13,7 +14,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
