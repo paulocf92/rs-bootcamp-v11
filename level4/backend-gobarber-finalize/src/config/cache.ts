c807b03ef6ac584fb8b89ad8650e4f8ee44bbfd7
Paulo Filho <pcf.feat@gmail.com>
2020-05-19 15:08:09 -0400

    feat: add express rate limit to mitigate brute-force requests

--- level4/backend-gobarber-finalize/src/config/cache.ts
+++ level4/backend-gobarber-finalize/src/config/cache.ts
@@ -12,9 +12,9 @@ export default {
   driver: 'redis',
   config: {
     redis: {
-      host: '192.168.99.100',
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
