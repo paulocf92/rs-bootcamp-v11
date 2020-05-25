d8caffbb05ff105002c7fdf40b77431eeca7edaa
Paulo Filho <pcf.feat@gmail.com>
2020-05-25 14:17:59 -0400

    chore: scaffold appointments list

--- level5/frontend-gobarber-finalize/src/pages/Dashboard/index.tsx
+++ level5/frontend-gobarber-finalize/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,8 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -62,6 +66,53 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/6351070?s=460&u=dd703c7a30cb2c826dd92c337457337b586c4fde&v=4"
+                  alt="Cliente"
+                />
+                <strong>Paulo Filho</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/6351070?s=460&u=dd703c7a30cb2c826dd92c337457337b586c4fde&v=4"
+                  alt="Cliente"
+                />
+                <strong>Paulo Filho</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/6351070?s=460&u=dd703c7a30cb2c826dd92c337457337b586c4fde&v=4"
+                  alt="Cliente"
+                />
+                <strong>Paulo Filho</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
