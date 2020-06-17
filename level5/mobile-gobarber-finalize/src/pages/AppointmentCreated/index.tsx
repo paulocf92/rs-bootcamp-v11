eae01dd9fca87dfb0fb3417d8225f754027d1265
Paulo Filho <pcf.feat@gmail.com>
2020-06-17 09:53:12 -0400

    chore: add success page

--- level5/mobile-gobarber-finalize/src/pages/AppointmentCreated/index.tsx
+++ level5/mobile-gobarber-finalize/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>
+        Terça, dia 14 de março de 2020 às 12:00h com Diego Fernandes.
+      </Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
