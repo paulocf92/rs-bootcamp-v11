eacc5d9c1e0dbea8ce7039cc8c3a7174eb3cf132
Paulo Filho <pcf.feat@gmail.com>
2020-06-17 13:17:42 -0400

    chore: add more small adjustments

--- level5/mobile-gobarber-finalize/src/pages/Profile/styles.ts
+++ level5/mobile-gobarber-finalize/src/pages/Profile/styles.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 0 30px 40px;
   position: relative;
 `;
 
@@ -16,7 +16,7 @@ export const Title = styled.Text`
 `;
 
 export const BackButton = styled.TouchableOpacity`
-  margin-top: ${Platform.OS === 'android' ? 150 : 40}px;
+  margin-top: 40px;
 `;
 
 export const UserAvatarButton = styled.TouchableOpacity`
