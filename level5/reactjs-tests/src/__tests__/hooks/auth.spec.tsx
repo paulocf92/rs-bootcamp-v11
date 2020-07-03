a62079768d244381421d49c894c4839d65fe734f
Paulo Filho <pcf.feat@gmail.com>
2020-07-03 10:44:00 -0400

    test: add api mock to sign-in tests

--- level5/reactjs-tests/src/__tests__/hooks/auth.spec.tsx
+++ level5/reactjs-tests/src/__tests__/hooks/auth.spec.tsx
@@ -1,9 +1,27 @@
 import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
+
 import { useAuth, AuthProvider } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
 
 describe('Auth hook', () => {
-  it('should be able to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('should be able to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user-123',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      token: 'token-123',
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -11,6 +29,17 @@ describe('Auth hook', () => {
       email: 'johndoe@example.com',
       password: '123456',
     });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
 
     expect(result.current.user.email).toBe('johndoe@example.com');
   });
